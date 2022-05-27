fb49e8c493f5dfcce63c087a6c835b2c8870eff2
veysiocal <veysiocal@hotmail.com>
2022-05-28 01:32:26 +0300

    adding modal

---
+++ Section 5 - Practicing The Basics/shared.js
@@ -0,0 +1,21 @@
+let planButtons = document.querySelectorAll('.plans-div button');
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+let modalConfirmButton = document.querySelector('.modal .modal__action');
+let modalCancelButton = document.querySelector('.modal__action--negative');
+
+planButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    })
+})
+
+let cancelButton = () => {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+modalCancelButton.addEventListener('click', cancelButton)
+
+backdrop.addEventListener('click', cancelButton)
